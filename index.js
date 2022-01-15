d030ca9ec2a707d2befa6a28bc59ec06555b053c
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-15 18:07:40 -0500

    commit tree 8c99b2ea7369b32a226dc2159398837138033d61

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number){
+   return number > 42 ? number - 42 : 42 - number 
+}
+distanceFromHqInBlocks()
+
+function distanceFromHqInFeet(number){
+    return distanceFromHqInBlocks(number) * 264
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet(start,end){
+return start > end ? (start - end) * 264 : (end - start) * 264 
+}
+
+distanceTravelledInFeet()
+
+function calculatesFarePrice(start,end) {
+    let dis = distanceTravelledInFeet(start,end) ;
+    if (dis <= 400) {
+        dis = 0;
+    } else if (dis > 400 && dis < 2000) {
+        dis =  (dis - 400) * .02 ;
+    } else if (dis > 2000 && dis < 2500) { 
+       dis = 25; 
+    } else { dis = 'cannot travel that far' ;}
+    return dis;
+}
+calculatesFarePrice()
